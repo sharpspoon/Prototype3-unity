90893c81dbeee0050c44193bbd49776c3dbaa192
sharpspoon <robin@steelcitysites.net>
2021-02-24 20:11:20 -0600

    3.2 done

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
-    // Start is called before the first frame update
+    private float repeatWidth;
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 50){
+        if (transform.position.x < startPos.x - repeatWidth){
             transform.position = startPos;
         }
     }
